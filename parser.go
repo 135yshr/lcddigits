2d8b0dac3ae05fba14c2913b086c98588c58fdc6
135yshr <isago@on-s.co.jp>
2015-10-01 20:20:34 +0900

    mod testcase remove to lf code

--- parser.go
+++ parser.go
@@ -3,23 +3,23 @@ package lcddigits
 func Parse(n int) string {
 	switch n {
 	case 0:
-		return "._.\n|.|\n|_|"
+		return "._.|.||_|"
 	case 1:
-		return "...\n..|\n..|"
+		return ".....|..|"
 	case 2:
-		return "._.\n._|\n|_."
+		return "._.._||_."
 	case 3:
-		return "._.\n._|\n._|"
+		return "._.._|._|"
 	case 5:
-		return "._.\n|_.\n._|"
+		return "._.|_.._|"
 	case 6:
-		return "._.\n|_.\n|_|"
+		return "._.|_.|_|"
 	case 7:
-		return "._.\n|.|\n..|"
+		return "._.|.|..|"
 	case 8:
 		return "._.|_||_|"
 	case 9:
-		return "._.\n|_|\n..|"
+		return "._.|_|..|"
 	}
 	return ""
 }
--- parser_test.go
+++ parser_test.go
@@ -16,42 +16,42 @@ var _ = Describe("#Parser", func() {
 		})
 		Context("数字の０が渡されたとき", func() {
 			It("LCDに表示する０が返ってくるべき", func() {
-				Expect(Parse(0)).To(Equal("._.\n|.|\n|_|"))
+				Expect(Parse(0)).To(Equal("._.|.||_|"))
 			})
 		})
 		Context("数字の６が渡されたとき", func() {
 			It("LCDに表示する６が返ってくるべき", func() {
-				Expect(Parse(6)).To(Equal("._.\n|_.\n|_|"))
+				Expect(Parse(6)).To(Equal("._.|_.|_|"))
 			})
 		})
 		Context("数字の５が渡されたとき", func() {
 			It("LCDに表示する５が返ってくるべき", func() {
-				Expect(Parse(5)).To(Equal("._.\n|_.\n._|"))
+				Expect(Parse(5)).To(Equal("._.|_.._|"))
 			})
 		})
 		Context("数字の９が渡されたとき", func() {
 			It("LCDに表示する９が返ってくるべき", func() {
-				Expect(Parse(9)).To(Equal("._.\n|_|\n..|"))
+				Expect(Parse(9)).To(Equal("._.|_|..|"))
 			})
 		})
 		Context("数字の７が渡されたとき", func() {
 			It("LCDに表示する７が返ってくるべき", func() {
-				Expect(Parse(7)).To(Equal("._.\n|.|\n..|"))
+				Expect(Parse(7)).To(Equal("._.|.|..|"))
 			})
 		})
 		Context("数字の３が渡されたとき", func() {
 			It("LCDに表示する３が返ってくるべき", func() {
-				Expect(Parse(3)).To(Equal("._.\n._|\n._|"))
+				Expect(Parse(3)).To(Equal("._.._|._|"))
 			})
 		})
 		Context("数字の２が渡されたとき", func() {
 			It("LCDに表示する２が返ってくるべき", func() {
-				Expect(Parse(2)).To(Equal("._.\n._|\n|_."))
+				Expect(Parse(2)).To(Equal("._.._||_."))
 			})
 		})
 		Context("数字の１が渡されたとき", func() {
 			It("LCDに表示する１が返ってくるべき", func() {
-				Expect(Parse(1)).To(Equal("...\n..|\n..|"))
+				Expect(Parse(1)).To(Equal(".....|..|"))
 			})
 		})
 	})
