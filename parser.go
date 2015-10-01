03d5f8da507e3a28a7eb955ab6463d6aff063060
135yshr <isago@on-s.co.jp>
2015-10-01 20:38:15 +0900

    mod refactor deleted switch

--- parser.go
+++ parser.go
@@ -15,28 +15,6 @@ var (
 	}
 )
 
-func Parse(n int) (ret []byte) {
-	switch n {
-	case 0:
-		ret = lcd_values[0]
-	case 1:
-		ret = lcd_values[1]
-	case 2:
-		ret = lcd_values[2]
-	case 3:
-		ret = lcd_values[3]
-	case 4:
-		ret = lcd_values[4]
-	case 5:
-		ret = lcd_values[5]
-	case 6:
-		ret = lcd_values[6]
-	case 7:
-		ret = lcd_values[7]
-	case 8:
-		ret = lcd_values[8]
-	case 9:
-		ret = lcd_values[9]
-	}
-	return
+func Parse(n int) []byte {
+	return lcd_values[n]
 }
